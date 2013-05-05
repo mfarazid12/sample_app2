41dd499b1604ec474b6ecb9c700eac9f7b7726e2
mfarazid@yahoo.com <mfarazid@yahoo.com>
2013-05-04 19:42:24 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '0e395de0287b56271af0493d508cea8a385f7407145f3d928e1d79a981f8fd6587cd24f755e13bfbd72ca7d2b06391d77e8e62644661f79327bd6314d01a7c1a'
---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Faraz Ali"
+    email    "mfarazid@yahoo.com"
+    password "foobar"
+    password_confirmation "foobar"
+  end
+end
